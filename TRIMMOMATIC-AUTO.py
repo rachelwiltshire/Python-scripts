313fdd1ddc14d21729f1d2f0616cb08c8bf360c6
Rachel Wiltshire <rwiltshi@nd.edu>
2017-01-27 15:04:44 -0500

    Create TRIMMOMATIC-AUTO.py

---
+++ TRIMMOMATIC-AUTO.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+#TRIMMOMATIC-AUTO.py#
+#Author: Rachel Wiltshire, U. Notre Dame, January 2017
+#Automated Trimmomatic usage for multiple samples - removes Illumina adapters and trims poor quality reads
+
+#Usage: Give X <arguments> in addition to the script.py in this format:
+        #TRIMMOMATIC-AUTO.py <input txt file> 
+
+#This script is specifically for the WGS gDNA PE libraries AFar April 2013 sequenced on Illumina HiSeq2000 v.1.9
+#Broad Institute (16 Anopheles genomes project)
+
+#Import default system
+import sys
+
+#If no arguments were given, print helpful message
+if len(sys.argv) != X:
+        print "Error! Give X arguments.\nUsage: python TRIMMOMATIC-AUTO.py <txt>"
+        sys.exit(0)
+
+module load bio
+
