7667e623d4d2c9f3e78c32984b9820cf56e94af5
Rachel Wiltshire <rwiltshi@nd.edu>
2017-02-20 14:02:02 -0500

    Update BWA-AUTO.py

--- BWA-AUTO.py
+++ BWA-AUTO.py
@@ -3,18 +3,28 @@
 #Author: Rachel Wiltshire, U. Notre Dame, February 2017
 #Automated BWA usage for multiple samples - alignment of reads to reference genome
 
-#Usage: Call python and BWA-AUTO.py
+#Usage: Call python and BWA-AUTO.py (and filename arguments if processing a range in a sorted directory list)
+        #i.e. python BWA-AUTO.py < file_1.paired.fq >
 
 #This script is specifically for the WGS gDNA PE libraries AFar April 2013 sequenced on Illumina HiSeq2000 v.1.9
 #Broad Institute (16 Anopheles genomes project)
 
 #Import dependencies
-import os, subprocess
+import os, subprocess, sys
+print sys.argv[1]
+#sys.exit(0)
 
-#Set variables
+#Set variables and logic statements
+if len(sys.argv) > 1:           #If an argument is given
+        processall = False      #Do not process until filename in sorted(os.listdir(indir)) = sys.argv[1]
+else: 
+        processall = True       #Process filename as per the FOR loop
+#print processall
+#sys.exit(0)
+
 indir = '/afs/crc.nd.edu/user/r/rwiltshi/FARAUTI/TERMINAL/trimmed/'
 outdir = '/afs/crc.nd.edu/user/r/rwiltshi/FARAUTI/TERMINAL/aligned/'
-bwa = '/opt/crc/bio/BWA/0.6.2/bin/bwa'    #v.0.6.2 is recommended for aln/sampe/samse #v.0.7.12 for mem
+bwa = '/opt/crc/bio/BWA/0.6.2/bin/bwa'          #v.0.6.2 is recommended for aln/sampe/samse #v.0.7.12 for mem
 samtools = '/opt/crc/bio/samtools/1.2.231.0/bin/samtools'
 AfarRef = '/afs/crc.nd.edu/user/r/rwiltshi/FARAUTI/TERMINAL/trimmed/Anopheles-farauti-FAR1_SCAFFOLDS_AfarF2.fa'
 
@@ -32,7 +42,7 @@ samtoolsCMD = samtools + " faidx" + space + AfarRef
 print samtoolsCMD
 subprocess.call(samtoolsCMD, shell=True)
 
-#Run through data files in directory and execute shell command when conditions are met
+#Run through filenames in directory and execute shell command when conditions are met
 #indir MUST be sorted first as os.listdir() is in arbitrary order as an artifact of the filesystem
 for filename in sorted(os.listdir(indir)):      
         #print filename
@@ -44,6 +54,14 @@ for filename in sorted(os.listdir(indir)):
         else:
                 continue
 
+        if (len(sys.argv) > 1 and sys.argv[1] == filename) or processall == True: #IF sys.argv[1] is given and its filename matches OR every file in listdir is being processed
+                if processall == False:                                           #If processall is set to False then it need resetting to True since we have satisfied the logic statement
+                        processall = True
+        else:
+                continue                                                          #If a range is being processed, and the filename does not match, continue to the next filename in listdir
+        print filename
+        #continue
+                
         if ((f1paired != "" and f2paired != "") and (f1paired[0:-11] == f2paired[0:-11])):
                 with open(f1paired) as f, open(f2paired) as g:
                         f1parts = f.name.rsplit("_", 1)
@@ -69,16 +87,4 @@ for filename in sorted(os.listdir(indir)):
                         f1paired = ""
                         f2paired = ""
                         
-#END
-
-#shell script
-#generate intermediate .sai files by aligning paired reads to reference genome
-bwa aln -t 12 -q 5 -l 32 -k 3 -n 9 -o 1 AfarRef SRX277192_Tanna2_run1-SRR849988-_1.paired.fq \
-> SRX277192_Tanna2_run1-SRR849988-_1.pe.aligned.sai
-bwa aln -t 12 -q 5 -l 32 -k 3 -n 9 -o 1 AfarRef SRX277192_Tanna2_run1-SRR849988-_2.paired.fq \
-> SRX277192_Tanna2_run1-SRR849988-_2.pe.aligned.sai
-#generate .sam file
-bwa sampe AfarRef SRX277192_Tanna2_run1-SRR849988-_1.pe.aligned.sai SRX277192_Tanna2_run1-SRR849988-_2.pe.aligned.sai \
-SRX277192_Tanna2_run1-SRR849988-_1.paired.fq SRX277192_Tanna2_run1-SRR849988-_2.paired.fq \
-> SRX277192_Tanna2_run1-SRR849988.pe.aligned.sam
 #END
